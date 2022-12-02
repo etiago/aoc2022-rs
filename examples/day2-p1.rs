bec7320642daa259d672109c6161f4af52477b5a
Tiago Espinha <tiagoespinha@microsoft.com>
2022-12-02 06:05:11 +0000

    Day 2

---
+++ examples/day2-p1.rs
@@ -0,0 +1,44 @@
+use std::{
+    collections::HashMap,
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let scoring_pairs = HashMap::from([
+        ("A", HashMap::from([("X", 3), ("Y", 6), ("Z", 0)])),
+        ("B", HashMap::from([("X", 0), ("Y", 3), ("Z", 6)])),
+        ("C", HashMap::from([("X", 6), ("Y", 0), ("Z", 3)])),
+    ]);
+
+    let scoring_per_type = HashMap::from([("X", 1), ("Y", 2), ("Z", 3)]);
+
+    let file = File::open("examples/inputs/day2.txt")?;
+    let reader = BufReader::new(file);
+
+    let mut score = 0;
+    reader.lines().for_each(|line| {
+        let line_str = line.expect("Failed to read line");
+        let play = line_str.split(" ").collect::<Vec<&str>>();
+
+        let elf_play = play[0];
+        let my_play = play[1];
+
+        let score_from_pairs = scoring_pairs
+            .get(elf_play)
+            .expect("Couldn´t find elf play")
+            .get(my_play)
+            .expect("Couldn´t find my play");
+
+        let score_from_type = scoring_per_type
+            .get(my_play)
+            .expect("Couldn´t find my play in per type");
+
+        score += score_from_pairs;
+        score += score_from_type;
+    });
+
+    println!("My score: {}", score);
+    Ok(())
+}
